8d828a4709e3869d4cc6531ad8a59d8137e172fc
Alexander Ulaev <ulaev.tlt@gmail.com>
2018-10-30 00:30:22 +0400

    vk

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { User } from '../components/User'
+import { Page } from '../components/Page'
+import { getPhotos } from '../actions/PageActions'
+import { handleLogin } from '../actions/UserActions'
+
+class App extends Component {
+  render() {
+    // вытащили handleLoginAction из this.props
+    const { user, page, getPhotosAction, handleLoginAction } = this.props
+    return (
+      <div className="app">
+        <Page
+          photos={page.photos}
+          year={page.year}
+          isFetching={page.isFetching}
+          getPhotos={getPhotosAction}
+        />
+        {/* добавили новые props для User */}
+        <User
+          name={user.name}
+          isFetching={user.isFetching}
+          error={user.error}
+          handleLogin={handleLoginAction}
+        />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = store => {
+  return {
+    user: store.user, // вытащили из стора (из редьюсера user все в переменную thid.props.user)
+    page: store.page,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getPhotosAction: year => dispatch(getPhotos(year)),
+    // "приклеили" в this.props.handleLoginAction функцию, которая умеет диспатчить handleLogin
+    handleLoginAction: () => dispatch(handleLogin()),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
